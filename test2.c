7f57ce771fa8361a3bf1d0761cccdd6a4f736e2b
gd1234567789 <46559776+gd1234567789@users.noreply.github.com>
2026-03-09 17:01:31 +0800

    冒泡排序

---
+++ test2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void bubble_sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main(void) {
+    int arr[] = {10, 3, 7, 4, 2, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Before sort: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    bubble_sort(arr, n);
+
+    printf("After sort:  ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
